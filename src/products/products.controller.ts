7849e7def618a70931aaadcd9aa759da15a8ba88
Nelson Cadenas <nca1478@yahoo.com>
2024-08-23 21:58:06 -0400

    agregado enum con nombres de mensajes TCP de productos (constans.ts)

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,32 +3,32 @@ import { Controller, ParseIntPipe } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PaginationDto } from '../common';
+import { PaginationDto, ProductTCP } from '../common';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   // @Post()
-  @MessagePattern({ cmd: 'create_product' })
+  @MessagePattern({ cmd: ProductTCP.CREATE })
   create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   // @Get()
-  @MessagePattern({ cmd: 'find_all_products' })
+  @MessagePattern({ cmd: ProductTCP.FIND_ALL })
   findAll(@Payload() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
   // @Get(':id')
-  @MessagePattern({ cmd: 'find_one_product' })
+  @MessagePattern({ cmd: ProductTCP.FIND_ONE })
   findOne(@Payload('id', ParseIntPipe) id: string) {
     return this.productsService.findOne(+id);
   }
 
   // @Patch(':id')
-  @MessagePattern({ cmd: 'update_product' })
+  @MessagePattern({ cmd: ProductTCP.UPDATE })
   update(
     // @Param('id', ParseIntPipe) id: string,
     // @Body() updateProductDto: UpdateProductDto,
@@ -38,7 +38,7 @@ export class ProductsController {
   }
 
   // @Delete(':id')
-  @MessagePattern({ cmd: 'delete_product' })
+  @MessagePattern({ cmd: ProductTCP.DELETE })
   remove(@Payload('id', ParseIntPipe) id: string) {
     return this.productsService.remove(+id);
   }
