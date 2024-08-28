b356b5de70b16676e842db4f10966a375073ebb5
Nelson Cadenas <nca1478@yahoo.com>
2024-08-28 17:49:36 -0400

    eliminada env var HOST

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,7 +4,6 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
-  HOST: string;
   NATS_SERVERS: string[];
 }
 
@@ -12,7 +11,6 @@ const envsSchema = joi
   .object({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
-    HOST: joi.string().required(),
     NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
@@ -31,6 +29,5 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   databaseUrl: envVars.DATABASE_URL,
-  host: envVars.HOST,
   natsServers: envVars.NATS_SERVERS,
 };
